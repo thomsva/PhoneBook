3544ea199427a34ce06d45f889252656e100c75a
thomsva <thomsva@gmail.com>
2022-05-04 19:28:21 +0300

    Update test

--- cypress/integration/phonebook_test.js
+++ cypress/integration/phonebook_test.js
@@ -1,7 +1,7 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:3000')
-    cy.contains('P')
-    cy.contains('N')
+    cy.contains('Puhelinluettelo')
+    cy.contains('Numerot')
   })
 })
